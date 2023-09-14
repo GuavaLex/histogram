7d5c6d489e01fb3297777dd9a773f281faf75d3b
guava <guavareigen2@gmail.com>
2023-09-14 11:19:05 +0530

    Daily histogram, cumulative histogram and daily line chart examples

--- histogram.py
+++ histogram.py
@@ -15,20 +15,28 @@ bin_edges = np.histogram_bin_edges(df['Line to Neutral Voltage'], bins=num_bins)
 
 # Create a histogram of the 'Line to Neutral Voltage' column using the same bin edges
 plt.figure(figsize=(10, 6))
-n, bins, patches = plt.hist(df['Line to Neutral Voltage'], bins=bin_edges, edgecolor='k', alpha=0.7)
+n, bins, patches = plt.hist(
+    df['Line to Neutral Voltage'],
+    bins=bin_edges,
+    edgecolor='k',
+    alpha=0.7
+)
 
 # Create labels for each bin indicating the bin width
-bin_widths = [f'{bins[i]:.2f}-{bins[i+1]:.2f}' for i in range(len(bins) - 1)]
+bin_widths = [f'Bin {i+1}: {bin_edges[i]:.2f}-{bin_edges[i+1]:.2f}' for i in range(len(bin_edges) - 1)]
 
 # Create a legend with bin width information
-legend_labels = [f'Bin {i+1}: {bin_width}' for i, bin_width in enumerate(bin_widths)]
-plt.legend(legend_labels)
+for i, bin_width in enumerate(bin_widths):
+    plt.bar(0, 0, color='white', label=bin_width, edgecolor='black', linewidth=1)
 
 # Format the chart
 plt.xlabel('Line to Neutral Voltage')
 plt.ylabel('Frequency')
 plt.title('Histogram of Line to Neutral Voltage with Specified Bin Width')
 plt.grid(True)
+
+# Create a legend
+plt.legend()
 
 # Display the plot
 plt.tight_layout()
